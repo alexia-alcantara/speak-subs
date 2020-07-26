f93a40286f2573cfdbb61eda1b1a3bb4cfbd1ef0
Allexia <alexialcantara@outlook.com>
2020-07-26 01:00:22 -0300

    front end da aplicação finalizada, footer atualizado

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { FaHeart, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
     return(
-        <p>Hello World!</p>
+        <footer className="footer">
+            <div className="footer__social">
+                <a href="https://www.instagram.com/speak.up.tech/">
+                <FaInstagram color="#ffff" size="40"/>
+                </a>
+
+                <a href="https://www.youtube.com/c/SpeakUpTech">
+                <FaYoutube color="#ffff" size="40"/>
+                </a>
+            </div>
+            <p className="footer__copyright">
+                Made with <FaHeart color="#851942"/> by Speak up Tech
+            </p>
+        </footer>
     )
 }
 
